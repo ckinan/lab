dc4aa755054b6b048a0bbfa707dd791c6dff316d
cesar <cesar.kina@gmail.com>
2026-08-22 12:15:53 -0500

    update pointers

--- scratchpad/c/pointers.c
+++ scratchpad/c/pointers.c
@@ -30,6 +30,13 @@ void print_int_bits(int value) {
 	printf("\n");
 }
 
+void increment(int *value) {
+	// *value = *value + 1; // valid
+	// *value++; // invalid, the compiler reads *value++ as *(value++), so
+	// 	what increments is the memory address, not the value itself
+	(*value)++;
+}
+
 int main() {
 	int number = 2022;
 	print_int_bytes(number);
@@ -38,4 +45,29 @@ int main() {
 	// %zu is the format specifier for type size_t
 	printf("an int uses %zu bytes of memory\n", sizeof(int));
 	// That prints "4" for me, but can vary by system.
+
+	printf("The value of number is %d ... And its address is %p\n", number,
+	       &number);
+
+	int a;
+	int *b;
+	b = &a;
+	a = 10;
+	// remember: b is a int-pointer, not the value itself
+	// the actual value is in variable `a`, not `b`
+	printf("step 1 :: a = %d, b = %d\n", a, *b);
+	*b = 20;
+	printf("step 2 :: a = %d, b = %d\n", a, *b);
+	printf("step 3 :: operate b + 10 = %d\n", (*b + 10));
+
+	increment(b);
+	printf("step 4 :: a-incremented = %d\n", a);
+	increment(&a);
+	printf("step 5 :: a-incremented = %d\n", a);
+
+	// int *bad = NULL;
+	// *bad = 12; // produces segmentation fault
+
+	printf("sizeof int = %zu :: sizeof int* = %zu :: sizeof int = %zu\n", sizeof(int),
+	       sizeof(b), sizeof(*b));
 }
